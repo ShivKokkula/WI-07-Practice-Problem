82607c594e305ff8d8e8b8a909e131b2d99422b9
ShivKokkula <shivbk987@gmail.com>
2021-06-20 18:12:10 +0530

    Refactor - added reset feature

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -75,3 +75,33 @@ function createAndUpdateStorage(employeePayRollData) {
     alert(employeePayRollList.toString());
     localStorage.setItem("EmployeePayRollList",JSON.stringify(employeePayRollData));
 }
+
+const resetForm = () => {
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setTextValue('#salary',30000);
+    setValue('#note','');
+    setValue('#day',1);
+    setValue('#month','January');
+    setValue('#year','2020');
+}
+
+const unsetSelectedValues = (propertyValue) =>{
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked  = false;
+    });
+}
+
+const setTextValue = (id,value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value  = value;
+}
